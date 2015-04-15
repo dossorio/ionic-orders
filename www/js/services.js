2cf0d5c95641f45d291092027a466b3b67ae6338
Daniel Ossorio <daniel@ossorio.me>
2015-04-15 09:10:13 +0100

    removed model dependency of the service

--- www/js/services.js
+++ www/js/services.js
@@ -1,89 +1,94 @@
-angular.module('starter.services', ['starter.config'])
-  .factory('DB', function($q, DB_CONFIG) {
-    var self = this;
-    self.db = null;
+var moduleServices = angular.module('starter.services', ['starter.config']);
 
-    self.init = function() {
+moduleServices.factory('DB', function($q, DB_CONFIG) {
+  var self = this;
+  self.db = null;
 
-      if (window.cordova) {
-        self.db = $cordovaSQLite.openDB(DB_CONFIG.name);
-      } else {
-        self.db = window.openDatabase(DB_CONFIG.name, '1.0', DB_CONFIG.description, -1);
-      }
+  self.init = function() {
 
-      angular.forEach(DB_CONFIG.tables, function(table) {
-        var columns = [];
+    if (window.cordova) {
+      self.db = $cordovaSQLite.openDB(DB_CONFIG.name);
+    } else {
+      self.db = window.openDatabase(DB_CONFIG.name, '1.0', DB_CONFIG.description, -1);
+    }
 
-        angular.forEach(table.columns, function(column) {
-          columns.push(column.name + ' ' + column.type);
-        });
+    angular.forEach(DB_CONFIG.tables, function(table) {
+      var columns = [];
 
-        var query = 'CREATE TABLE IF NOT EXISTS ' + table.name + ' (' + columns.join(',') + ')';
-
-        self.query(query);
-        console.log('Table ' + table.name + ' initialized');
-      });
-    };
-
-    self.query = function(query, bindings) {
-      bindings = typeof bindings !== 'undefined' ? bindings : [];
-      var deferred = $q.defer();
-
-      self.db.transaction(function(transaction) {
-        transaction.executeSql(query, bindings, function(transaction, result) {
-          deferred.resolve(result);
-        }, function(transaction, err) {
-          deferred.reject(err);
-        });
+      angular.forEach(table.columns, function(column) {
+        columns.push(column.name + ' ' + column.type);
       });
 
-      return deferred.promise;
-    };
+      var query = 'CREATE TABLE IF NOT EXISTS ' + table.name + ' (' + columns.join(',') + ')';
 
-    self.fetchOne = function(result){
-      return result.rows.item(0);
-    };
+      self.query(query);
+      console.log('Table ' + table.name + ' initialized');
+    });
+  };
 
-    self.fetchAll = function(result) {
-      var items = [];
+  self.query = function(query, bindings) {
+    bindings = typeof bindings !== 'undefined' ? bindings : [];
+    var deferred = $q.defer();
 
-      for (var i = 0; i < result.rows.length; i++) {
-        items.push(result.rows.item(i));
-      }
+    self.db.transaction(function(transaction) {
+      transaction.executeSql(query, bindings, function(transaction, result) {
+        deferred.resolve(result);
+      }, function(transaction, err) {
+        deferred.reject(err);
+      });
+    });
 
-      return items;
-    };
+    return deferred.promise;
+  };
 
-    return self;
-  })
+  self.fetchOne = function(result){
+    return result.rows.item(0);
+  };
 
-  .factory('Customer', function(DB) {
+  self.fetchAll = function(result) {
+    var items = [];
+
+    for (var i = 0; i < result.rows.length; i++) {
+      items.push(result.rows.item(i));
+    }
+
+    return items;
+  };
+
+  return self;
+});
+
+
+(function(){
+  var modelName = 'Customer';
+
+  var modelService = function(DB) {
     var self = this;
+    console.log(modelName);
 
-    self.create = function(customer) {
+    self.create = function(modelObj) {
       var columns = [];
       var values = [];
 
-      for (attr in customer) {
+      for (attr in modelObj) {
         columns.push("'" + attr + "'");
-        values.push("'" + customer[attr] + "'");
+        values.push("'" + modelObj[attr] + "'");
       }
 
-      var query = 'INSERT INTO Customer (' + columns.join(',') + ') VALUES (' + values.join(',') + ')';
+      var query = 'INSERT INTO '+ modelName +' (' + columns.join(',') + ') VALUES (' + values.join(',') + ')';
 
       return DB.query(query);
     };
 
     self.getAll = function() {
-      return DB.query('SELECT * FROM Customer')
+      return DB.query('SELECT * FROM '+ modelName)
         .then(function(result) {
-          console.log('ALL', result);
           return DB.fetchAll(result);
         });
     };
 
     self.getById = function(id) {
-      var query = 'SELECT * FROM Customer WHERE id = ' + id;
+      var query = 'SELECT * FROM '+ modelName +' WHERE id = ' + id;
       return DB.query(query)
         .then(function(result){
           return DB.fetchOne(result);
@@ -91,4 +96,7 @@ angular.module('starter.services', ['starter.config'])
     };
 
     return self;
-  });
+  };
+
+  moduleServices.factory(modelName, modelService);
+})();
